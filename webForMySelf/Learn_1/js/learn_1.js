639a69b39c9a588c122097154ca53894e53b7196
VelesDragon <ScorpionEvil777@gmai.com>
2024-06-21 08:39:50 +0300

    Повторение 3 блока

--- webForMySelf/Learn_1/js/learn_1.js
+++ webForMySelf/Learn_1/js/learn_1.js
@@ -18,3 +18,11 @@ console.log(h1)
 
 let div2 = document.querySelector('div#playground ul') //Получение вложенного элемента
 console.log(div2)
+
+console.log(div.innerHTML) // Вывод информаци в консоль из блока div с помощью innerHTML
+div.innerHTML = '<p style="color: gray">Новый текст </p>' // Вывод текста на страницу innerHtml
+console.log(h1.textContent) // Вывод информаци в консоль из блока h1 textContent
+h1.textContent = 'Динамическое изменение текста в H1' // Выводи текста на страницу с помощью textContent
+
+let inputText = document.querySelector('.inputText')
+console.log(inputText.value) // С помощью value мы получаем доступ к значению input
